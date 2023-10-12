81172dff8f0d3ea288dfbd47e9908f6861dcc353
Mohammad <moreb.mohammad@gmail.com>
2023-10-12 14:07:56 +0300

    Update Introduction.py

--- 03. Data Structures/Arrays/Introduction.py
+++ 03. Data Structures/Arrays/Introduction.py
@@ -10,6 +10,23 @@
 #Insert - O(n)
 #Delete - O(n)
 
+# Sum Func 
+
+
+def sum(i1, i2):
+    result = 0
+    for i in range(i1, i2 + 1):
+        result += i
+    return result
+
+def main():
+    print("Sum from 1 to 10 is", sum(1, 10)) 
+    print("Sum from 20 to 37 is", sum(20, 37))
+    print("Sum from 35 to 49 is", sum(35, 49))
+
+main() # Call the main function
+
+
 array = [5,8,2,9,17,43,25,100]
 
 #Look-up/Acces
